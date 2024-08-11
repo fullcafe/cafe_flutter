783a84cf2a38ec8f88c8cee88726da116d700cea
leaf1191 <jwlv111@naver.com>
2024-08-11 22:27:46 +0900

    feat: 로그인 후 페이지 레이아웃 및 프로바이더 생성

--- lib/views/login/after_login.dart
+++ lib/views/login/after_login.dart
@@ -1,12 +1,63 @@
+import 'package:cafe_front/constants/colors.dart';
+import 'package:cafe_front/views/login/set_age_page.dart';
+import 'package:cafe_front/views/login/set_nickname_page.dart';
+import 'package:cafe_front/widgets/button/custom_button_layout.dart';
 import 'package:flutter/material.dart';
 
-class AfterLogin extends StatelessWidget {
+class AfterLogin extends StatefulWidget {
   const AfterLogin({Key? key}) : super(key: key);
+
+  @override
+  State<AfterLogin> createState() => _AfterLoginState();
+}
+
+class _AfterLoginState extends State<AfterLogin> {
+  final pages = const [SetNicknamePage(), SetAgePage()];
+  final pageController = PageController();
+  var currentPage = 1;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Text('data'),
+      appBar: AppBar(
+        leading: IconButton(
+            onPressed: (){
+              pageController.animateToPage(currentPage - 1, duration: const Duration(milliseconds: 500), curve: Curves.ease);
+            },
+            highlightColor: Colors.transparent,
+            icon: const Icon(Icons.arrow_back_ios)
+        ),
+      ),
+      body: SafeArea(
+          child: Column(
+            children: [
+              Expanded(flex: 6,
+                  child: PageView.builder(
+                      controller: pageController,
+                      physics: const NeverScrollableScrollPhysics(),
+                      itemCount: pages.length,
+                      itemBuilder: (context, i){
+                        return pages[i];
+                      }
+                  )
+              ),
+              // 다음 버튼
+              Expanded(
+                  child: GestureDetector(
+                    onTap: (){
+                      pageController.animateToPage(currentPage + 1, duration: const Duration(milliseconds: 500), curve: Curves.ease);//fdf
+                    },
+                    child: const CustomButtonLayout(
+                      margin: EdgeInsets.fromLTRB(10, 25, 10, 25),
+                      width: double.infinity,
+                      borderColor: CustomColors.deepGrey,
+                      child: Center(child: Text('다음',style: TextStyle(fontWeight: FontWeight.bold,color: CustomColors.deepGrey),)),
+                    ),
+                  )
+              )
+            ],
+          )
+      ),
     );
   }
 }
--- lib/views/login/login_main.dart
+++ lib/views/login/login_main.dart
@@ -7,71 +7,74 @@ class LoginMain extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        children: [
-          // 상반
-          Expanded(flex: 7,child: Container(
-            child: Column(
-              children: [
-                Expanded(child: Container()),
-                const Expanded(child:
-                Text('당신의 입맛과 취향을\n     저격할 카페를\n   추천해드릴게요.',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)
-                )
-              ],
-            ),
-          )),
-          // 하반
-          Expanded(
-              flex:2,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // 상반
+            Expanded(flex: 7,child: Container(
               child: Column(
                 children: [
-                  // 로그인/회원가입
-                  const SizedBox(
-                    height: 20,
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 10),
-                              child: Divider(),
-                            )
-                        ),
-                        Text('로그인/회원가입',style: TextStyle(color: Colors.grey),),
-                        Expanded(
-                            child: Padding(
-                              padding: EdgeInsets.symmetric(horizontal: 10),
-                              child: Divider(),
-                            )
-                        ),
-                      ],
-                    ),
-                  ),
-                  // 구글 버튼
-                  Expanded(flex:3,child: GestureDetector(
-                    onTap: (){
-                      Navigator.pushNamedAndRemoveUntil(context, '/login/after',ModalRoute.withName('/'));
-                    },
-                    child: CustomButtonLayout(
-                      margin: const EdgeInsets.fromLTRB(10, 25, 10, 25),
-                      width: double.infinity,
-                      child: Stack(
+                  Expanded(child: Container()),
+                  const Expanded(child:
+                  Text('당신의 입맛과 취향을\n     저격할 카페를\n   추천해드릴게요.',style: TextStyle(fontWeight: FontWeight.bold,fontSize: 30),)
+                  )
+                ],
+              ),
+            )),
+            // 하반
+            Expanded(
+                flex:2,
+                child: Column(
+                  children: [
+                    // 로그인/회원가입
+                    const SizedBox(
+                      height: 20,
+                      child: Row(
                         children: [
-                          Container(
-                              margin: const EdgeInsets.symmetric(horizontal: 10),
-                              width: 30,
-                              child: Center(child: Image.asset('assets/icons/google_logo.png',scale: 0.1,))
+                          Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                child: Divider(),
+                              )
                           ),
-                          const Center(child: Text('Google로 계속하기',style: TextStyle(fontWeight: FontWeight.bold),))
+                          Text('로그인/회원가입',style: TextStyle(color: Colors.grey),),
+                          Expanded(
+                              child: Padding(
+                                padding: EdgeInsets.symmetric(horizontal: 10),
+                                child: Divider(),
+                              )
+                          ),
                         ],
                       ),
                     ),
-                  )),
-                  // 로그인 하기(하드)
-                  const Expanded(flex:2,child: Text('로그인 하기',style: TextStyle(fontWeight: FontWeight.bold),))
-                ],
-              )
-          )
-        ],
+                    // 구글 버튼
+                    Expanded(flex:3,child: GestureDetector(
+                      onTap: (){
+                        // 로그인 후 유저가 등록 되었느냐에 따라 이동하는 페이지가 다름
+                        Navigator.pushNamedAndRemoveUntil(context, '/login/after',ModalRoute.withName('/'));
+                      },
+                      child: CustomButtonLayout(
+                        margin: const EdgeInsets.fromLTRB(10, 25, 10, 25),
+                        width: double.infinity,
+                        child: Stack(
+                          children: [
+                            Container(
+                                margin: const EdgeInsets.symmetric(horizontal: 10),
+                                width: 30,
+                                child: Center(child: Image.asset('assets/icons/google_logo.png',scale: 0.1,))
+                            ),
+                            const Center(child: Text('Google로 계속하기',style: TextStyle(fontWeight: FontWeight.bold),))
+                          ],
+                        ),
+                      ),
+                    )),
+                    // 로그인 하기(하드)
+                    const Expanded(flex:2,child: Text('로그인 하기',style: TextStyle(fontWeight: FontWeight.bold),))
+                  ],
+                )
+            )
+          ],
+        ),
       ),
     );
   }
