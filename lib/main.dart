5982fcdd6fb83012a76e2446b65745fceb800a55
leaf1191 <jwlv111@naver.com>
2024-08-13 13:56:15 +0900

    feat: 구글 auth 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,25 @@
 import 'package:cafe_front/constants/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  // 테스트용 리스너
+  FirebaseAuth.instance
+      .authStateChanges()
+      .listen((User? user) {
+    if (user == null) {
+      print('User is currently signed out!');
+    } else {
+      print('User is signed in!');
+    }
+  });
+
   runApp(MaterialApp(
       initialRoute: '/login',
       routes: routes,
