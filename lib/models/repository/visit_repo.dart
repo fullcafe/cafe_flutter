afedaef609c09da3b2a8ca79ea79b7c2b78d0c08
leaf1191 <jwlv111@naver.com>
2024-11-08 22:56:58 +0900

    refactor: 방문 repository 임시 리팩토링

--- lib/models/repository/visit_repo.dart
+++ lib/models/repository/visit_repo.dart
@@ -1,25 +1,27 @@
-import 'package:dio/dio.dart';
+import 'dart:convert';
+
+import 'package:cafe_front/services/api_service.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 import '../visit.dart';
 
 class VisitRepository {
-  final Dio _dio;
-
-  VisitRepository(this._dio);
+  final ApiService _apiService = ApiService();
 
   // 방문 기록 생성 메서드
   Future<void> createVisit(Visit visit) async {
+    var data = visit.toJson();
+    var encodedData = jsonEncode(data);
     try {
-      await _dio.post('/visit', data: visit.toJson());
+      await _apiService.postRequest('/visit', encodedData);
     } catch (e) {
-      print('Error creating visit: $e');
-      throw Exception('Failed to create visit');
+      Fluttertoast.showToast(msg: '주문에 실패했습니다.');
     }
   }
 
   // 특정 사용자 ID로 모든 방문 기록을 가져오는 메서드
   Future<List<Visit>> findAllVisitByUser(String uid) async {
     try {
-      final response = await _dio.get('/visits/all/$uid');
+      final response = await _apiService.getRequest('/visits/all/$uid');
       List data = response.data as List;
       return data.map((json) => Visit.fromJson(json)).toList();
     } catch (e) {
@@ -31,7 +33,7 @@ class VisitRepository {
   // 리뷰 작성 가능한 방문 기록을 가져오는 메서드
   Future<List<Visit>> findWriteReviewVisitByUser(String uid) async {
     try {
-      final response = await _dio.get('/visits/write_review/$uid');
+      final response = await _apiService.getRequest('/visits/write_review/$uid');
       List data = response.data as List;
       return data.map((json) => Visit.fromJson(json)).toList();
     } catch (e) {
@@ -43,7 +45,7 @@ class VisitRepository {
   // 방문 횟수가 가장 많은 기록을 가져오는 메서드
   Future<List<Visit>> findMostCountVisitByUser(String uid) async {
     try {
-      final response = await _dio.get('/visits/most_visit/$uid');
+      final response = await _apiService.getRequest('/visits/most_visit/$uid');
       List data = response.data as List;
       return data.map((json) => Visit.fromJson(json)).toList();
     } catch (e) {
