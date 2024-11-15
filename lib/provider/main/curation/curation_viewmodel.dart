a47fbe1804ba29888328430f37316387a2f87a9a
NIsik419 <81894972+NIsik419@users.noreply.github.com>
2024-11-16 00:45:21 +0900

    refactor:curationPage

--- lib/provider/main/curation/curation_viewmodel.dart
+++ lib/provider/main/curation/curation_viewmodel.dart
@@ -2,6 +2,7 @@ import 'package:cafe_front/models/cafe.dart';
 import 'package:cafe_front/models/cafe_filter.dart';
 import 'package:cafe_front/models/repository/cafe_repo.dart';
 import 'package:flutter/material.dart';
+import '../../../common/char_keyword.dart';
 import '../../../common/user_store.dart';
 
 class CurationViewModel with ChangeNotifier {
@@ -14,15 +15,7 @@ class CurationViewModel with ChangeNotifier {
   List<Cafe>? get preferredCafes => _preferredCafes;
   List<Cafe>? get cafes => _cafes;
 
-  // 캐릭터 인덱스별 선호 키워드 리스트
-  static const charKeywords = [
-    ['아메리카노','라떼','커피','디저트','마들렌'],
-    ['조용한', '아메리카노', '분위기','산책','대화'],
-    ['아메리카노', '원두', '요거트','소금빵','휘낭시에'],
-    ['아메리카노', '크림', '분위기','마들렌','과일'],
-    ['아메리카노', '요거트', '차', '디카페인'],
-    ['아메리카노', '라떼', '그린티','케이크','분위기'],
-  ];
+
 
   Future<void> fetchData() async {
     // 필터 생성
