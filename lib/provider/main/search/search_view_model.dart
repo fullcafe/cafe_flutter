304ff93b85d0e633899e4d7ecbe270103a44f703
leaf1191 <jwlv111@naver.com>
2024-11-23 10:35:39 +0900

    feat: search page 이전 방문 카페 api 연동

--- lib/provider/main/search/search_view_model.dart
+++ lib/provider/main/search/search_view_model.dart
@@ -1,3 +1,6 @@
+import 'package:cafe_front/common/user_store.dart';
+import 'package:cafe_front/models/complex_visit.dart';
+import 'package:cafe_front/models/repository/visit_repo.dart';
 import 'package:cafe_front/provider/stack_handler.dart';
 import 'package:flutter/material.dart';
 import 'package:cafe_front/models/cafe.dart';
@@ -7,15 +10,27 @@ import 'package:provider/provider.dart';
 
 class SearchViewModel with ChangeNotifier {
   final CafeRepository _cafeRepository = CafeRepository();
+  final VisitRepository _visitRepository = VisitRepository();
   final StackHandler _handler = StackHandler();
   // search view는 하나의 controller를 공유
   final TextEditingController searchController = TextEditingController();
+  
+  SearchViewModel(){
+    fetchPreviousCafe();
+  }
 
+  List<ComplexVisit>? _previousCafe;
   List<Cafe> _cafeResults = [];
   bool _isLoading = false;
 
+  List<ComplexVisit>? get previousCafe => _previousCafe;
   List<Cafe> get cafeResults => _cafeResults;
   bool get isLoading => _isLoading;
+
+  fetchPreviousCafe() async {
+    _previousCafe = await _visitRepository.findWriteReviewVisitByUser(UserStore.getInstance().user!.uid);
+    notifyListeners();
+  }
 
   /// 검색어를 통해 카페를 검색하는 메서드
   Future<void> searchCafes(String keyword) async {
